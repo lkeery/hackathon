7113d066b4782ea63b4affb9837b2d43cd3a013e
lkeery <l_keery@fanshaweonline.ca>
2019-10-07 18:53:37 -0400

    Added JS toggle and render images, style updates

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,20 @@
 (() => {
-    // test fired
-    console.log('fired!');
+    // Test
+    console.log('Connected!');
+
+    const toggle = document.querySelector('.button');
+    const img = document.querySelector('.render');
+    let x = 1;
+
+    function toggleMode() {
+        if (x == 1) {
+            img.src = `images/dark-mode.png`;
+            x = 2;
+        } else if (x == 2) {
+            img.src = `images/light-mode.png`;
+            x = 1;
+        }
+    }
+
+    toggle.addEventListener('click', toggleMode);
 })()
